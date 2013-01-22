e6fcfa55304f288f71f6ce9ab06b67b61cd5eec0
Nick Fisher <nfisher@trafficland.com>
2013-01-22 13:09:23 -0500

    added jenkins automation scripts

---
+++ src/main/g8/scripts/jenkins/add_to_ci.rb
@@ -0,0 +1,23 @@
+require 'rest_client'
+require 'open-uri'
+
+job_name = "$name$"
+user = "$jenkins_user$"
+password = "$jenkins_password$"
+credentials = CGI::escape(user) + ":" + CGI::escape(password)
+jenkins = "http://#{credentials}@build01.tl.com:8080"
+
+['master', 'develop'].each do |branch|
+  config = IO.read("./config-#{branch}.xml")
+
+  url = "#{jenkins}/createItem?name=#{job_name}"
+  puts url
+  response = RestClient.post url, config, :content_type => :xml, :accept => :xml
+  unless response.code == 200
+    puts response.to_str 
+  else
+    puts "Created job for #{job_name} successfully"
+    
+  end   
+
+end  
