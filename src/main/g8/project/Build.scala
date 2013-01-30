47943a5731d076f61f0ab9b72c7935497a086e5b
Nick Fisher <nfisher@trafficland.com>
2013-01-30 18:45:15 -0500

    use TL plugins and jenkins credentials

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -1,12 +1,13 @@
 import sbt._
 import Keys._
-import play.Project._
+import trafficland.opensource.sbt.plugins._
+import trafficland.sbt.plugins._
 
 object ApplicationBuild extends Build {
   import Dependencies._
 
   val appName         = "$name$"
-  val appVersion      = "0.1.0-SNAPSHOT"
+  val appVersion      = "0.1.0-SNAPSHOT".toReleaseFormat
 
   resolvers ++= resolutionRepos
 
@@ -14,80 +15,16 @@ object ApplicationBuild extends Build {
 
   lazy val UnitTest = config("unit") extend Test
 
-  val dist = TaskKey[File]("dist", "Build the standalone application package")
-  val distTask = (distDirectory, baseDirectory, playPackageEverything, dependencyClasspath in Runtime, target, normalizedName, version) map {
-    (dist, root, packaged, dependencies, target, id, version) =>
-      val packageName = id + "-" + version
-      val packageDirectory = packageName + "/" + id
-      val zip = dist / (packageName + ".zip")
-
-      IO delete dist
-      IO createDirectory dist
-
-      val libs = {
-        dependencies.filter(_.data.ext == "jar").map { dependency =>
-          val filename = for {
-            module <- dependency.metadata.get(AttributeKey[ModuleID]("module-id"))
-            artifact <- dependency.metadata.get(AttributeKey[Artifact]("artifact"))
-          } yield {
-            module.organization + "." + module.name + "-" + Option(artifact.name.replace(module.name, "")).filterNot(_.isEmpty).map(_ + "-").getOrElse("") + module.revision + ".jar"
-          }
-          val path = ("lib/" + filename.getOrElse(dependency.data.getName))
-          dependency.data -> path
-        } ++ packaged.map(jar => jar -> ("/lib/" + jar.getName))
-      }
-
-      val start = target / "start"
-
-      val customConfig = Option(System.getProperty("config.file"))
-      val customFileName = customConfig.map(f => Some((new File(f)).getName)).getOrElse(None)
-
-      IO.write(start,
-        """#!/usr/bin/env sh
-scriptdir=`dirname \$0`
-classpath=""" + libs.map { case (jar, path) => "\$scriptdir/" + path }.mkString("\"", ":", "\"") + """
-exec /opt/java \$* -cp \$classpath """ + customFileName.map(fn => "-Dconfig.file=`dirname \$0`/conf/" + fn + " ").getOrElse("-Dconfig.file=`dirname \$0`/conf/application.conf ") + """play.core.server.NettyServer `dirname \$0`
-                                                                                                                                                                                """ /* */ )
-      val scripts = Seq(start -> (packageDirectory + "/start"))
-
-      val other = Seq((root / "README") -> (packageDirectory + "/README"))
-
-      val productionConfig = customFileName.map(fn => target / fn).getOrElse(target / "application.conf")
-
-      val prodApplicationConf = customConfig.map { location =>
-        val customConfigFile = new File(location)
-        IO.copyFile(customConfigFile, productionConfig)
-        Seq(productionConfig -> (packageDirectory + "/conf/" + customConfigFile.getName))
-      }.getOrElse(Nil)
-
-      val defaultApplicationConf = Seq(new File("conf/application.conf") -> (packageDirectory + "/conf/application.conf"))
-
-      IO.zip(libs.map { case (jar, path) => jar -> (packageDirectory + "/" + path) } ++ scripts ++ other ++ prodApplicationConf ++ defaultApplicationConf, zip)
-      IO.delete(start)
-      IO.delete(productionConfig)
-
-      println()
-      println(appName + " has been packaged.  The package can be found at " + zip.getCanonicalPath + "!")
-      println()
-
-      zip
-  }
-
   val main = play.Project(
     appName,
     appVersion,
     appDependencies
   )
+  .settings(Play20PluginSet.plugs : _*)
   .configs(UnitTest)
   .settings(inConfig(UnitTest)(Defaults.testTasks) : _*)
-  .settings(defaultScalaSettings:_*)
   .settings(
-    scalaVersion := "$scala_version$",
     resolvers ++= resolutionRepos,
-    sourceGenerators in Compile <+= sourceManaged in Compile map { outDir: File =>
-      writeVersion(outDir)
-    },
-    dist <<= distTask,
     testOptions in Test := Seq(
       Tests.Setup { () => System.setProperty("config.file", "conf/test.conf") }
     ),
@@ -96,20 +33,9 @@ exec /opt/java \$* -cp \$classpath """ + customFileName.map(fn => "-Dconfig.file
       Tests.Filter { _.contains(".unit.") }
     ),
     parallelExecution in Test := false,
-    parallelExecution in UnitTest := false
+    parallelExecution in UnitTest := false,
+    sbt.Keys.fork in Test := false
   )
-
-  def writeVersion(outDir: File) = {
-    val file = outDir / "AppInfo.scala"
-    IO.write(file,
-    """package controllers
-    object AppInfo {
-      val version = "%s"
-      val name = "%s"
-      val vendor = "TrafficLand, Inc."
-    }""".format(appVersion, appName))
-    Seq(file)
-  }
 }
 
 object Dependencies {
@@ -120,7 +46,7 @@ object Dependencies {
   object V {
     val tlcommons = "1.2"
     val reactive  = "0.8"
-    val scalatest = "$scalatest_version$"
+    val scalatest = "2.0.M5b"
   }
 
   val compileDeps = Seq(
--- src/main/g8/project/plugins.sbt
+++ src/main/g8/project/plugins.sbt
@@ -3,3 +3,7 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % "2.1-RC2")
+
+resolvers += Resolver.url("TrafficLand Artifactory Plugins Server", url("http://build01.tl.com:8081/artifactory/repo"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("com.trafficland" % "tl-sbt-plugins" % "0.6.6")
